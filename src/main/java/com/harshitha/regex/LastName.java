c8f2a8ea8f718df554f99f4231cf44495e47b00e
HarshithaG069 <harshithag069@gmail.com>
2021-10-22 11:54:26 +0530

    Added:LastName

--- src/main/java/com/harshitha/regex/LastName.java
+++ src/main/java/com/harshitha/regex/LastName.java
@@ -1,2 +1,15 @@
-package com.harshitha.regex;public class LastName {
+package com.harshitha.regex;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class LastName {
+    public static void main(String[] args) {
+        Pattern pattern = Pattern.compile("^[A-Z]{1}[a-z]{2,}");
+        Matcher matcher = pattern.matcher("Chigulla");
+        if (matcher.find())
+            System.out.println("Pattern matched");
+        else
+            System.out.println("Pattern  not matched");
+    }
 }
