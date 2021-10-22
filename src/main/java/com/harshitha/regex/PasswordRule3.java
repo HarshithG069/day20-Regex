fc7f36dd8b71005e6e14e5fe559831c4129773eb
HarshithaG069 <harshithag069@gmail.com>
2021-10-22 12:46:44 +0530

    Added:PasswordRule3

--- src/main/java/com/harshitha/regex/PasswordRule3.java
+++ src/main/java/com/harshitha/regex/PasswordRule3.java
@@ -3,10 +3,10 @@ package com.harshitha.regex;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class PasswordRule2 {
+public class PasswordRule3 {
     public static void main(String[] args) {
-        Pattern pattern = Pattern.compile("^[a-z](?=.*[A-Z]).{8,}$");
-        Matcher matcher = pattern.matcher("harshiTha");
+        Pattern pattern = Pattern.compile("(?=.*[A-Z])(?=.*[0-9])");
+        Matcher matcher = pattern.matcher("harshiTha1");
         if (matcher.find())
             System.out.println("Pattern matched");
         else
