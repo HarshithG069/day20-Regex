479f600d6f267aff013c3ffcb12b1e16049ddc32
HarshithaG069 <harshithag069@gmail.com>
2021-10-22 12:31:22 +0530

    Added:PasswordRule1

--- src/main/java/com/harshitha/regex/PasswordRule1.java
+++ src/main/java/com/harshitha/regex/PasswordRule1.java
@@ -3,10 +3,10 @@ package com.harshitha.regex;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class MobileNumber {
+public class PasswordRule1 {
     public static void main(String[] args) {
-        Pattern pattern = Pattern.compile("^[91 ][6-9][0-9]{9}$");
-        Matcher matcher = pattern.matcher("91 8296702715");
+        Pattern pattern = Pattern.compile("^[a-z]{8,}$");
+        Matcher matcher = pattern.matcher("harshitha");
         if (matcher.find())
             System.out.println("Pattern matched");
         else
